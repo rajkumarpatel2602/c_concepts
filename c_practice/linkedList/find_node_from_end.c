a7747f8698a1a58bc49d150913a5295b9b24af58
Rajkumar <rajkumar.patel@seagate.com>
2021-09-10 22:50:09 +0530

    Add find from list's end example

--- c_practice/linkedList/find_node_from_end.c
+++ c_practice/linkedList/find_node_from_end.c
@@ -34,9 +34,30 @@ void add_ll(node_t **head, int data){
 	}
 }
 
+int find_node(node_t *head, int pos){
+	node_t *back = head, *front = head;
+	int cnt = 0;
+	while(cnt < pos){
+		cnt++;
+		//front can point to last node at most
+		//return in case it reaches to NULL
+		if((front = front->next) == NULL) return -1;
+	}
+
+	printf("cnt is %d\n", cnt);
+
+	//increament front till the last node
+	while(front = front->next){
+		//start progressing with back pointer too
+	 	back = back->next;
+	}
+	return back->data;
+}
+
 node_t *head= NULL;
 
 int main(int argc, char* argv[]){
+	int pos = -1;
 	add_ll(&head, 10);
 	add_ll(&head, 20);
 	add_ll(&head, 30);
@@ -44,4 +65,12 @@ int main(int argc, char* argv[]){
 	add_ll(&head, 50);
 	add_ll(&head, 60);
 	print_ll(head);
+	printf("Enter the position from the last to get that node's data :\n");
+	scanf("%d", &pos);
+	int ret = find_node(head, pos);
+	if(-1 == ret){
+		printf("given position is not present\n");
+	}else{
+		printf("Required node-data is : %d\n", ret);
+	}
 }
