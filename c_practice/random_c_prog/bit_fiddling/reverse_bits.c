f704df96f67c56a8bfce1fc73dc60f25f456dd8c
Rajkumar <rajkumar.patel@seagate.com>
2021-09-19 19:42:34 +0530

    Fix reverse bit function

--- c_practice/random_c_prog/bit_fiddling/reverse_bits.c
+++ c_practice/random_c_prog/bit_fiddling/reverse_bits.c
@@ -4,16 +4,14 @@ int main(){
     int data = 0x1010; //
     int i;
     printf("data before : ");
-    for(i = 0; i < 16; i++){
+    for(i = 15; i >= 0; i--){
         printf("%d", (data>>i) & 0x1);
     }
     printf("\n");
     printf("data after : ");
     data = data ^ 0xffff;
-    for(i = 0; i < 16; i++){
+    for(i = 15; i >= 0; i--){
         printf("%d", (data>>i) & 0x1);
     }
     printf("\n");
-
-
 }
